419913deb0267b2a4d5294a3d07272921869cdf1
trifel <>
2018-11-22 18:00:26 +0100

    change property file handling, send udp test packge

--- README.md
+++ README.md
@@ -4,7 +4,13 @@ ixi plugin for ict to report current node information. The data is used for visu
 
 ## Installing
 
+### Step 1
+
 ```
 $ cd Ict/ixi
 $ git clone https://github.com/trifel/Report.ixi.git
 ```
+
+### Step 2
+
+Create and configure the file `report.properties` in the Ict root directory. You can use the sample file `report.properties.sample`.
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var Runnable = Java.type("java.lang.Runnable");
 var Timer = Java.type('java.util.Timer');
 var InetAddress = Java.type('java.net.InetAddress');
+var InetSocketAddress = Java.type('java.net.InetSocketAddress');
 var Properties = Java.type('java.util.Properties');
 var FileInputStream = Java.type('java.io.FileInputStream');
-//var FileOutputStream = Java.type('java.io.FileOutputStream');
+var FileOutputStream = Java.type('java.io.FileOutputStream');
+var OutputStreamWriter = Java.type('java.io.OutputStreamWriter');
 var DatagramPacket = Java.type('java.net.DatagramPacket');
 var DatagramSocket = Java.type('java.net.DatagramSocket');
 var UUID = Java.type('java.util.UUID');
 
-var PROPERTY_FILE = "ixi/Report.ixi/report.properties";
+var PROPERTY_FILE = "report.properties";
 
 var reportTimer;
 var reportServerHost = "0.0.0.0";
@@ -16,6 +18,9 @@ var reportServerPort = "14245";
 
 var nodeName = "";
 var nodeUUID = "";
+var nodeSocketAddress = "0.0.0.0";
+var nodeSocketPort = "14244";
+var nodeSocket = null;
 
 function init() {
   try {
@@ -29,8 +34,7 @@ function init() {
 function loadProperties() {
   var properties = new Properties();
   var propertiesInputStream = new FileInputStream(PROPERTY_FILE);
-  //var propertiesOutputStream = new FileOutputStream(PROPERTY_FILE);
-
+  
   properties.load(propertiesInputStream);
 
   nodeName = properties.getProperty("nodeName", nodeName).trim();
@@ -41,9 +45,19 @@ function loadProperties() {
   if (nodeUUID !== null) {
     nodeUUID = nodeUUID.trim();
   } else {
-    nodeUUID = UUID.randomUUID();
+    nodeUUID = UUID.randomUUID().toString();
     properties.put("nodeUUID", nodeUUID);
-    //properties.save(propertiesOutputStream);
+
+    try {
+      var outputStreamWriter = new OutputStreamWriter(new FileOutputStream(PROPERTY_FILE, true));
+      var data = "\nnodeUUID = " + nodeUUID;
+      outputStreamWriter.append(data);
+      outputStreamWriter.flush();
+      outputStreamWriter.close();
+    } catch (e) {
+      print(e);
+    }
+
   }
 }
 
@@ -57,6 +71,22 @@ function initReportTimer() {
     }
 
     //TODO send node status to report server
+    var message = nodeUUID;
+    var messageByteArray = message.getBytes();
+
+    var packet = new DatagramPacket(messageByteArray, messageByteArray.length);
+    nodeSocket = new DatagramSocket(nodeSocketPort, InetAddress.getByName(nodeSocketAddress));
+    
+    var reportServerAddress = new InetSocketAddress(reportServerHost, reportServerPort);
+    
+    packet.setSocketAddress(reportServerAddress);
+    nodeSocket.send(packet);
+
+    try {
+      if (!nodeSocket.isClosed()) nodeSocket.close();
+    } catch(exception) {
+      print(exception);
+    }
 
   }, 0, 60000);
 }
@@ -64,6 +94,13 @@ function initReportTimer() {
 init();
 
 IXICycle.put("shutdown", new Runnable(function () {
+  if (nodeSocket != undefined) {
+    try {
+      if (!nodeSocket.isClosed()) nodeSocket.close();
+    } catch(exception) {
+      print(exception);
+    }
+  }
   if (reportTimer != undefined) {
     reportTimer.cancel();
     reportTimer.purge();
