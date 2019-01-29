c16c03e2cc52e77f5ea1e37ad4506078cb06b89c
Anton Wiklund <ixuz07@gmail.com>
2019-01-29 19:02:52 +0100

    Adopting new configuration framework of Ict

--- src/main/java/com/ictreport/ixi/utils/Properties.java
+++ src/main/java/com/ictreport/ixi/utils/Properties.java
@@ -27,19 +27,7 @@ public class Properties extends java.util.Properties {
     private final static String DEFAULT_NEIGHBORS = "";
 
     public Properties() {
-        setRequiredProps();
-    }
 
-    public Properties(final String propertiesFilePath) {
-
-        File propertiesLocation = new File(Constants.PROPERTIES_LOCATION);
-        if (propertiesLocation.mkdirs()) {
-            LOGGER.info(String.format("Created properties folder at: %s\n", propertiesLocation.getAbsolutePath()));
-        }
-
-        load(propertiesFilePath);
-        setRequiredProps();
-        validateProps();
     }
 
     /**
@@ -98,85 +86,6 @@ public class Properties extends java.util.Properties {
         put(NAME, name);
     }
 
-    public List<InetSocketAddress> getNeighborAddresses() {
-        return neighborsFromString(getProperty(NEIGHBORS));
-    }
-
-    public void load(final String propertiesFilePath) {
-        FileInputStream inputStream = null;
-        try {
-            inputStream = new FileInputStream(propertiesFilePath);
-            load(inputStream);
-        } catch (final FileNotFoundException e) {
-            LOGGER.info(String.format("Could not read properties file '%s', therefore a new one will be created.",
-                    propertiesFilePath));
-        } catch (final IOException e) {
-            LOGGER.error(String.format("Failed to open input stream of file: '%s'",
-                    propertiesFilePath));
-            e.printStackTrace();
-        } finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    LOGGER.error(String.format("Failed to close input stream of file: '%s'",
-                            propertiesFilePath));
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    public void store(final String propertiesFilePath) {
-        FileOutputStream outputStream = null;
-        try {
-            outputStream = new FileOutputStream(propertiesFilePath);
-            store(outputStream, null);
-        } catch (final FileNotFoundException e) {
-            LOGGER.error(String.format("Failed to open output stream of file: '%s'",
-                    propertiesFilePath));
-            e.printStackTrace();
-        } catch (final IOException e) {
-            LOGGER.error(String.format("Failed to save properties to file: '%s'",
-                    propertiesFilePath));
-            e.printStackTrace();
-        } finally {
-            if (outputStream != null) {
-                try {
-                    outputStream.close();
-                } catch (IOException e) {
-                    LOGGER.error(String.format("Failed to close output stream of file: '%s'",
-                            propertiesFilePath));
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    private void setRequiredProps() {
-        if (get(NAME) == null) {
-            put(NAME, DEFAULT_NAME);
-        }
-        if (get(REPORT_PORT) == null) {
-            put(REPORT_PORT, Integer.toString(DEFAULT_REPORT_PORT));
-        }
-        if (get(NEIGHBORS) == null) {
-            put(NEIGHBORS, DEFAULT_NEIGHBORS);
-        }
-    }
-
-    private void validateProps() {
-        if (!getName().matches(".+\\s\\(ict-\\d+\\)")) {
-            try {
-                throw new InvalidPropertiesFormatException(String.format("Unable to set %s. Please follow the naming convention: %s",
-                        NAME,
-                        "\"<name> (ict-<number>)\""));
-            } catch (InvalidPropertiesFormatException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
     @SuppressWarnings("unchecked")
     public Enumeration keys() {
         final Enumeration keysEnum = super.keys();
@@ -186,37 +95,5 @@ public class Properties extends java.util.Properties {
         }
         Collections.sort(keyList);
         return keyList.elements();
-    }
-
-    private static List<InetSocketAddress> neighborsFromString(final String string) {
-        final List<String> addresses = stringListFromString(string);
-
-        final List<InetSocketAddress> neighbors = new LinkedList<>();
-        for (final String address : addresses) {
-            try {
-                neighbors.add(inetSocketAddressFromString(address));
-            } catch (final Throwable t) {
-                LOGGER.error(String.format("Invalid neighbor address: '%s'", address));
-            }
-        }
-        return neighbors;
-    }
-
-    private static List<String> stringListFromString(final String string) {
-        final List<String> stringList = new LinkedList<>();
-        for (final String element : string.split(LIST_DELIMITER)) {
-            if (element.length() == 0)
-                continue;
-            stringList.add(element);
-        }
-        return stringList;
-    }
-
-    private static InetSocketAddress inetSocketAddressFromString(final String address) {
-        int portColonIndex;
-        for (portColonIndex = address.length() - 1; address.charAt(portColonIndex) != ':'; portColonIndex--);
-        final String hostString = address.substring(0, portColonIndex);
-        final int port = Integer.parseInt(address.substring(portColonIndex + 1, address.length()));
-        return new InetSocketAddress(hostString, port);
     }
 }
--- src/main/java/org/iota/ict/ixi/ReportIxi.java
+++ src/main/java/org/iota/ict/ixi/ReportIxi.java
@@ -11,28 +11,34 @@ import com.ictreport.ixi.utils.Properties;
 import java.io.File;
 import java.net.InetSocketAddress;
 import java.security.KeyPair;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.iota.ict.ixi.context.ConfigurableIxiContext;
+import org.iota.ict.ixi.context.IxiContext;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ReportIxi extends IxiModule {
 
     private final static Logger LOGGER = LogManager.getLogger(ReportIxi.class);
-    private Properties properties;
+    private final ReportIxiContext context = new ReportIxiContext();
+    private final Properties properties = new Properties();
     private Metadata metadata;
-    private final List<Neighbor> neighbors = new LinkedList<>();
+    private List<Neighbor> neighbors = new LinkedList<>();
     private Api api;
     private KeyPair keyPair;
     public final Object waitingForUuid = new Object();
-    public byte state = STATE_TERMINATED;
-    public final static byte STATE_TERMINATED = 0;
-    public final static byte STATE_INITIALIZING = 1;
-    public final static byte STATE_RUNNING = 2;
-    public final static byte STATE_TERMINATING = 3;
+    private byte state = STATE_TERMINATED;
+    private final static byte STATE_TERMINATED = 0;
+    private final static byte STATE_INITIALIZING = 1;
+    private final static byte STATE_RUNNING = 2;
+    private final static byte STATE_TERMINATING = 3;
 
     public ReportIxi(final Ixi ixi) {
         super(ixi);
+        context.applyConfiguration();
     }
 
     @Override
@@ -50,18 +56,8 @@ public class ReportIxi extends IxiModule {
         state = STATE_INITIALIZING;
 
         LOGGER.info(String.format("Report.ixi %s: Starting...", Constants.VERSION));
-        properties = new Properties(Constants.PROPERTIES_FILE);
-        properties.store(Constants.PROPERTIES_FILE);
         metadata = new Metadata(Constants.METADATA_FILE);
         keyPair = Cryptography.generateKeyPair(Constants.KEY_LENGTH);
-
-        LOGGER.info("Assigning neighbor addresses...");
-        for (final InetSocketAddress neighborAddress : properties.getNeighborAddresses()) {
-            neighbors.add(new Neighbor(neighborAddress));
-            if (neighbors.size() > 3) {
-                throw new RuntimeException("Too many neighbors in properties!");
-            }
-        }
 
         LOGGER.info("Initiating API...");
         api = new Api(this);
@@ -109,5 +105,107 @@ public class ReportIxi extends IxiModule {
 
     public boolean isRunning() {
         return state == STATE_RUNNING;
+    }
+
+    @Override
+    public IxiContext getContext() {
+        return context;
+    }
+
+    private class ReportIxiContext extends ConfigurableIxiContext {
+
+        private static final String FIELD_NAME = "name";
+        private static final String FIELD_NEIGHBORS = "neighbors";
+        private static final String FIELD_REPORT_PORT = "reportPort";
+
+        protected ReportIxiContext() {
+            super(new JSONObject()
+                    .put(FIELD_NAME, "name (ict-1)")
+                    .put(FIELD_REPORT_PORT, "1338")
+                    .put(FIELD_NEIGHBORS, new JSONArray(neighbors)));
+        }
+
+        @Override
+        protected void validateConfiguration(JSONObject newConfiguration) {
+            validateName(newConfiguration);
+            validateNeighbors(newConfiguration);
+            validateReportPort(newConfiguration);
+        }
+
+        @Override
+        protected void applyConfiguration() {
+            properties.setName(configuration.getString(FIELD_NAME));
+            properties.setReportPort(configuration.getInt(FIELD_REPORT_PORT));
+            JSONArray neighborsArray = configuration.getJSONArray(FIELD_NEIGHBORS);
+            neighbors = new ArrayList<>();
+            for(Object neighbor : neighborsArray.toList())
+                neighbors.add(new Neighbor(inetSocketAddressFromString((String) neighbor)));
+        }
+
+        private void validateName(final JSONObject newConfiguration) {
+
+            if (!newConfiguration.has(FIELD_NAME)) {
+                throw new IllegalPropertyException(FIELD_NAME, "not defined");
+            }
+            if (!(newConfiguration.get(FIELD_NAME) instanceof String)) {
+                throw new IllegalPropertyException(FIELD_NAME, "not a string");
+            }
+            if (!newConfiguration.getString(FIELD_NAME).matches(".+\\s\\(ict-\\d+\\)")) {
+                throw new IllegalPropertyException(FIELD_NAME,
+                        "please follow the naming convention: \"<name> (ict-<number>)\"");
+            }
+        }
+
+        private void validateNeighbors(final JSONObject newConfiguration) {
+
+            if (!newConfiguration.has(FIELD_NEIGHBORS)) {
+                throw new IllegalPropertyException(FIELD_NEIGHBORS, "not defined");
+            }
+            if(!(newConfiguration.get(FIELD_NEIGHBORS) instanceof JSONArray)) {
+                throw new IllegalPropertyException(FIELD_NEIGHBORS, "not an array");
+            }
+
+            JSONArray array = newConfiguration.getJSONArray(FIELD_NEIGHBORS);
+
+            if (array.length() > 3) {
+                throw new IllegalPropertyException(FIELD_NEIGHBORS, "maximum 3 neighbors allowed");
+            }
+
+            for(int i = 0; i < array.length(); i++) {
+                if (!(array.get(i) instanceof String)) {
+                    throw new IllegalPropertyException(FIELD_NEIGHBORS, "array element at index " + i + " is not a string");
+                }
+
+                inetSocketAddressFromString((String)array.get(i));
+            }
+        }
+
+        private void validateReportPort(final JSONObject newConfiguration) {
+
+            if (!newConfiguration.has(FIELD_REPORT_PORT)) {
+                throw new IllegalPropertyException(FIELD_REPORT_PORT, "not defined");
+            }
+            if (!(newConfiguration.get(FIELD_REPORT_PORT) instanceof Integer)) {
+                throw new IllegalPropertyException(FIELD_REPORT_PORT, "not an integer");
+            }
+            if (newConfiguration.getInt(FIELD_REPORT_PORT) < 0 || newConfiguration.getInt(FIELD_REPORT_PORT) > 65535) {
+                throw new IllegalPropertyException(FIELD_REPORT_PORT, "port must be within range 0-65535");
+            }
+        }
+
+        private InetSocketAddress inetSocketAddressFromString(final String address) {
+            int portColonIndex;
+            for (portColonIndex = address.length() - 1; address.charAt(portColonIndex) != ':'; portColonIndex--);
+            final String hostString = address.substring(0, portColonIndex);
+            final int port = Integer.parseInt(address.substring(portColonIndex + 1, address.length()));
+            return new InetSocketAddress(hostString, port);
+        }
+    }
+
+    private class IllegalPropertyException extends IllegalArgumentException {
+
+        private IllegalPropertyException(String field, String cause) {
+            super("Invalid property '"+field+"': " + cause + ".");
+        }
     }
 }
