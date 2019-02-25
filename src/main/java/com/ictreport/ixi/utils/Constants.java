000676970f1fcc9ab8a4cfba32c8569f4e0793cb
Anton Wiklund <ixuz07@gmail.com>
2019-02-25 15:16:50 +0100

    Merged

--- src/main/java/com/ictreport/ixi/utils/Constants.java
+++ src/main/java/com/ictreport/ixi/utils/Constants.java
@@ -2,7 +2,7 @@ package com.ictreport.ixi.utils;
 
 public class Constants {
 
-    public static final String VERSION = "0.6-SNAPSHOT";
+    public static final String VERSION = "0.5.1-SNAPSHOT";
     public static final String RCS_HOST = "api.ictreport.com";
     public static final int    RCS_PORT = 14265;
     public static final String TAG = "REPORT9IXI99999999999999999";
