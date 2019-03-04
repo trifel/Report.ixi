6e5f8c1f76aa46a74efb985b4db64120236be84e
Anton Wiklund <ixuz07@gmail.com>
2019-03-04 16:39:57 +0100

    0.5.2

--- src/main/java/com/ictreport/ixi/utils/Constants.java
+++ src/main/java/com/ictreport/ixi/utils/Constants.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Constants {
 
-    public static final String VERSION = "0.5.2-SNAPSHOT";
+    public static final String VERSION = "0.5.2";
     public static final String RCS_HOST = "api.ictreport.com";
     public static final int    RCS_PORT = 14265;
     public static final String TAG = "REPORT9IXI99999999999999999";
@@ -14,7 +14,7 @@ public class Constants {
     public static final java.io.File METADATA_DIRECTORY = new java.io.File("modules/report.ixi/");
     public static final String METADATA_FILE = PROPERTIES_LOCATION+"/report.ixi.metadata";
     public static final String REPORT_IXI_CONFIG_FILE = "modules/report.ixi-" + Constants.VERSION + ".jar.cfg";
-    private static final List<String> previousVersions = Arrays.asList("0.5.1", "0.5.1-SNAPSHOT", "0.5");
+    private static final List<String> previousVersions = Arrays.asList("0.5.2-SNAPSHOT", "0.5.1", "0.5.1-SNAPSHOT", "0.5");
 
     public static List<String> getPreviousVersions() {
         return new ArrayList<>(previousVersions);
